c0edf36003807687f54eb7ec57e77164cad1b2c2
denisholly <denisholly@jozefholly.com>
2022-06-23 15:28:07 +0200

    Project Setup

---
+++ src/components/a-home-header.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>aldsjfkasdjf</h1>
+</template>
---
+++ src/views/home-page.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <a-home-header />
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonContent } from '@ionic/vue';
+import AHomeHeader from '@/components/a-home-header.vue';
+
+export default  defineComponent({
+  name: 'home-page',
+  components: { AHomeHeader, IonHeader, IonToolbar, IonContent, IonPage }
+});
+</script>
